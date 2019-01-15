652ebcfc0ce3a468465d82bd5a80e2fcabf90502
Tina Wu <tinawu.nd23@gmail.com>
2019-01-14 21:33:30 -0800

    saw this quote on the OS reading and decided I wanna make a script that adds quote to a file so i could use it on my homepage later

---
+++ quotes/quotes.py
@@ -0,0 +1,22 @@
+# a simply terminal program that adds quotes i like to a text file
+
+import sys
+import json
+
+print('Enter quotes below, press q to quit:')
+
+with open('quotes\quotes.json') as f:
+    quotesdict = json.load(f)
+
+index = int(sorted(list(quotesdict.keys()))[-1])
+
+quotes = sys.stdin.readline()
+while quotes.rstrip() != 'q':
+    index += 1
+    quotesdict[str(index)] = quotes.rstrip()
+    print('Quote added!\n')
+    quotes = sys.stdin.readline()
+
+print(quotesdict)
+with open('quotes\quotes.json', 'w') as f:
+    json.dump(quotesdict, f)
